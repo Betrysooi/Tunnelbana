de5d3f819dc1e2e494e51281c1abc197dcde5bc0
Betrysooi <133110237+Betrysooi@users.noreply.github.com>
2024-03-06 20:10:37 +0100

    Add files via upload

---
+++ Tunnelbana_uppgift/script.js
@@ -0,0 +1,12 @@
+var stations = ['Hagsätra', 'Rågsved', 'Högdalen', 'Bandhagen', 'Stureby', 'Svedmyra', 'Sockenplan', 'Enskede Gård', 'Globen', 'Gullmarsplan', 'Skanstull', 'Medborgarplatsen', 'Slussen', 'Gamla Stan', 'T-Centralen', 'Hötorget', 'Rådmansgatan',
+'Odenplan', 'S:T Eriksplan', 'Fridhemsplan', 'Thorildsplan', 'Kristineberg', 'Alvik', 'Stora Mossen', 'Abrahamsberg', 'Brommaplan', 'Åkeshov', 'Ängbyplan', 'Islandstorget', 'Blackeberg', 'Råcksta', 'Vällingby', 'Johannelund', 'Hässelby Gård', 'Hässelby Strand'];
+
+var option = "";
+
+for(var i = 0; i < stations.length; i++)
+{
+    option += '<option value="'+ stations[i] + '">' + stations[i] + "</option>"
+}
+
+var from = document.getElementById('station').innerHTML = option;
+var to = document.getElementById('stations').innerHTML = option;
